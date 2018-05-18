7eb7756eb7e4e59da442d70dcd486a9a1332f0dd
Robert Choi <robertmatthewchoi@gmail.com>
2018-05-17 22:14:26 -0400

    Starting work on board/pieces
    Writing pawn logic and board spaces scripts

---
+++ Assets/Scripts/Pieces/Pawn.cs
@@ -0,0 +1,35 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Pawn : MonoBehaviour {
+
+	public bool isWhite;
+	private int moveIncrement;
+	private Transform position;
+	private int[] coordinates = new int[2];
+	private GameObject chessBoard = GameObject.Find("Board");
+	private script boardScript = chessBoard.GetComponent("ChessBoard");
+
+	// Use this for initialization
+	void Start () {
+		moveIncrement = isWhite ? 1 : -1;
+	}
+	
+	// Update is called once per frame
+	void Update () {
+		
+	}
+
+	void OnMouseDown () {
+
+		boardScript.selectedPiece = this;
+
+		boardScript.unhighlightSpaces();
+
+		boardScript.boardSpaces[coordinates[0], coordinates[1] + moveIncrement].highlighted = true;
+		if ((isWhite && coordinates [1] == 1) || (!isWhite && coordinates [1] == 6)) {
+			boardScript.boardSpaces[coordinates[0], coordinates[1] + (moveIncrement * 2)].highlighted = true;
+		}
+	}
+}
---
+++ Assets/Scripts/Space.cs
@@ -0,0 +1,22 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Space : MonoBehaviour {
+
+	public bool highlighted;
+
+	// Use this for initialization
+	void Start () {
+		highlighted = false;
+	}
+	
+	// Update is called once per frame
+	void Update () {
+		
+	}
+
+	void OnMouseDown () {
+
+	}
+}
